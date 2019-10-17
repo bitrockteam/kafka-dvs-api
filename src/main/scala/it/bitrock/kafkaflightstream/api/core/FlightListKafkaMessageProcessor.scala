5fc9d7ad117bec2675215cdbcc1dd8f276ff7039
Massimo Siani <massimo.siani@bitrock.it>
2019-10-17 17:40:39 +0200

    check non empty kafka results

--- src/main/scala/it/bitrock/kafkaflightstream/api/core/FlightListKafkaMessageProcessor.scala
+++ src/main/scala/it/bitrock/kafkaflightstream/api/core/FlightListKafkaMessageProcessor.scala
@@ -19,25 +19,23 @@ class FlightListKafkaMessageProcessor(
     kafkaConsumerWrapperFactory: KafkaConsumerWrapperFactory
 ) extends KafkaMessageProcessor {
 
-  private val cache: FlightReceivedList = FlightReceivedList(Seq())
-
   override val kafkaConsumerWrapper: KafkaConsumerWrapper =
     kafkaConsumerWrapperFactory.build(self, List(kafkaConfig.flightReceivedListTopic))
 
-  override def receive: Receive = {
+  override def receive: Receive = active(FlightReceivedList(Seq()))
+
+  def active(cache: FlightReceivedList): Receive = {
     case NoMessage =>
       logger.debug("Got no-message notice from Kafka Consumer, going to poll again")
       kafkaConsumerWrapper.pollMessages()
 
     case flights: FlightReceivedList =>
       logger.debug(s"Got a $flights from Kafka Consumer")
-      cache.copy(elements = flights.elements)
+      if (flights.elements.nonEmpty) context.become(active(flights))
       throttle(kafkaConsumerWrapper.pollMessages())
 
     case UpdateRequested => sender ! cache
 
     case Terminated => self ! PoisonPill
   }
-
-  def child(props: Props): ActorRef = context.actorOf(props)
 }
