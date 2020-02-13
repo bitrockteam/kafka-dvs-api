ed5ebffc9a08542b24991ed2f49e03ec5c9382a5
Scala Steward <me@scala-steward.org>
2020-02-13 16:09:21 +0100

    Update pureconfig to 0.12.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -24,7 +24,7 @@ object Dependencies {
     lazy val TestCommons         = "0.0.8"
     lazy val KafkaCommons        = "0.0.8"
     lazy val LogbackClassic      = "1.2.3"
-    lazy val PureConfig          = "0.12.1"
+    lazy val PureConfig          = "0.12.2"
     lazy val ScalaLogging        = "3.9.2"
     lazy val Slf4j               = "1.7.28"
     lazy val Mockito             = "2.7.22"
