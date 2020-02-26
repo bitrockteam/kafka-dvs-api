a27a8821158460a9e8245dc489c5c7812b8a32ec
Daniele Bonelli <daniele.bonelli92@gmail.com>
2020-02-26 16:03:00 +0100

    scalafix

--- src/test/scala/it/bitrock/dvs/api/TestProbeExtensions.scala
+++ src/test/scala/it/bitrock/dvs/api/TestProbeExtensions.scala
@@ -2,9 +2,9 @@ package it.bitrock.dvs.api
 
 import akka.testkit.TestProbe
 
-import scala.concurrent.duration.FiniteDuration
-
 object TestProbeExtensions {
+  import scala.concurrent.duration.FiniteDuration
+
   implicit class TestKitOps(tk: TestProbe) {
     def expectMessage[T](obj: T)(implicit timeout: FiniteDuration): T = tk.expectMsg(timeout, obj)
   }
