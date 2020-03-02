493fa63425f194c5c4f0658b83b4d87333e51df7
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2020-03-02 14:20:24 +0100

    Update scalacheck-shapeless_1.14 to 1.2.5 (#126)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -29,7 +29,7 @@ object Dependencies {
     lazy val Slf4j               = "1.7.30"
     lazy val Mockito             = "3.3.0"
     lazy val ScalafixSortImports = "0.3.2"
-    lazy val ScalacheckShapeless = "1.2.4"
+    lazy val ScalacheckShapeless = "1.2.5"
     lazy val ScalaTestAutofix    = "3.1.0.0"
     lazy val ScalaTestPlus       = "3.1.0.0"
 
