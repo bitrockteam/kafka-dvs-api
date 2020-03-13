6ec7fc73d1ca2854d0d12301d6ed54045a95c546
Scala Steward <me@scala-steward.org>
2020-03-13 18:16:14 +0100

    Update akka-stream, akka-stream-testkit to 2.6.4

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -13,7 +13,7 @@ object Dependencies {
 
   object Versions {
     lazy val Scala               = "2.12.10"
-    lazy val Akka                = "2.6.3"
+    lazy val Akka                = "2.6.4"
     lazy val AkkaHttp            = "10.1.11"
     lazy val AkkaHttpCors        = "0.4.2"
     lazy val ConfluentPlatform   = "5.4.0"
