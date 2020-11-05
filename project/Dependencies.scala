6216a0d710b700b730a63d96bd718c9ba4962ea3
Scala Steward <me@scala-steward.org>
2020-11-05 18:16:10 +0100

    Update akka-http-cors to 1.1.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -15,7 +15,7 @@ object Dependencies {
     lazy val Scala               = "2.12.10"
     lazy val Akka                = "2.6.10"
     lazy val AkkaHttp            = "10.1.12"
-    lazy val AkkaHttpCors        = "0.4.3"
+    lazy val AkkaHttpCors        = "1.1.0"
     lazy val ConfluentPlatform   = "5.4.3"
     lazy val EmbeddedKafka       = "5.4.1.2"
     lazy val JakartaWsRs         = "2.1.6"
