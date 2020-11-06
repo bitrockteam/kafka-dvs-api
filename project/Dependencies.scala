d234497048ebd8f9e973853be8b5ebd591bb94fe
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2020-11-06 12:46:15 +0100

    Update scalacheck-1-14 to 3.1.4.0 (#192)
    * Update scalacheck-1-14 to 3.1.4.0
    
    * Update scalacheck-1-14 to 3.1.4.0
    
    Co-authored-by: Matteo Gazzetta <matteo.gazzetta@bitrock.it>

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -31,7 +31,7 @@ object Dependencies {
     lazy val ScalafixSortImports = "0.5.5"
     lazy val ScalacheckShapeless = "1.2.5"
     lazy val ScalaTestAutofix    = "3.1.0.1"
-    lazy val ScalaTestPlus       = "3.1.1.1"
+    lazy val ScalaTestPlus       = "3.1.4.0"
     lazy val Cats                = "2.2.0"
   }
 
