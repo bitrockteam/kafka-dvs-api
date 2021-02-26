79c39264da625fbdff54c1d23c2410808697e80e
Scala Steward <me@scala-steward.org>
2021-02-26 22:24:23 +0100

    Update scala-library to 2.12.13

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -12,7 +12,7 @@ object Dependencies {
   }
 
   object Versions {
-    lazy val Scala               = "2.12.10"
+    lazy val Scala               = "2.12.13"
     lazy val Akka                = "2.6.13"
     lazy val AkkaHttp            = "10.2.3"
     lazy val AkkaHttpCors        = "1.1.1"
