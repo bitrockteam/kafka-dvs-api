5a22d3f8310a66133302852bc006c124b7c1bee8
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2021-12-14 20:37:08 +0100

    Update akka-http, akka-http-spray-json, ... to 10.2.7 (#306)
    Co-authored-by: Matteo Gazzetta <matteo.gazzetta@bitrock.it>

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -14,7 +14,7 @@ object Dependencies {
   object Versions {
     lazy val Scala               = "2.12.15"
     lazy val Akka                = "2.6.17"
-    lazy val AkkaHttp            = "10.2.3"
+    lazy val AkkaHttp            = "10.2.7"
     lazy val AkkaHttpCors        = "1.1.2"
     lazy val ConfluentPlatform   = "5.4.5"
     lazy val EmbeddedKafka       = "5.4.1.2"
