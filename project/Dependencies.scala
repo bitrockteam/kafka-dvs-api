83e617d57bbc6618139f4b6bc4d5747a54af3a50
Scala Steward <me@scala-steward.org>
2022-03-29 00:05:20 +0200

    Update kafka-avro-serializer, ... to 5.4.7

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -16,7 +16,7 @@ object Dependencies {
     lazy val Akka                = "2.6.18"
     lazy val AkkaHttp            = "10.2.7"
     lazy val AkkaHttpCors        = "1.1.2"
-    lazy val ConfluentPlatform   = "5.4.6"
+    lazy val ConfluentPlatform   = "5.4.7"
     lazy val EmbeddedKafka       = "5.4.1.2"
     lazy val JakartaWsRs         = "2.1.6"
     lazy val Kafka               = "2.4.1"
